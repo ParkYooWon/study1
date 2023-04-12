66d7e1f576fd6577cf1e28ceb5e64f80d966fd0a
박유원 <130129986+ParkYooWon@users.noreply.github.com>
2023-04-12 19:48:43 +0900

    Add files via upload

---
+++ EX2.java
@@ -0,0 +1,21 @@
+package DAY0412;
+
+public class EX2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=5;
+		while (a<10)
+		{
+			a++;
+			if (a>7)
+			{
+				continue;
+			}
+			System.out.println(a);
+			
+		}
+	}
+
+}
